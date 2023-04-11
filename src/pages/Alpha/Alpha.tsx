c829d49d1b8db1e1862a614dd0dad78e91c23547
Henry YP Ho <me@h3nr7.com>
2023-04-11 22:48:00 +0100

    cleaned

--- src/pages/Alpha/Alpha.tsx
+++ src/pages/Alpha/Alpha.tsx
@@ -1,11 +1,5 @@
-import { Canvas, extend, Object3DNode, useThree } from '@react-three/fiber'
 import { PerspectiveCamera, OrbitControls, Environment, Sky, Shadow, shaderMaterial } from '@react-three/drei'
 import { FiberWrapper } from '../../three/components/FiberWrapper'
-import { usePotreeLoader } from '../../three/loaders/usePotreeLoader'
-import { BackSide, Color, DirectionalLight, Euler, FrontSide, Material, PlaneGeometry, Quaternion, Texture, Vector3 } from 'three'
-import { useTextureLoader } from '../../three/loaders/useTextureLoader'
-import { useLayoutEffect, useMemo } from 'react'
-import { degToRad } from 'three/src/math/MathUtils'
 import { CanvasControl } from '../../three/components/CanvasControl'
 import './materials/ColorShiftMaterial';
 import { Island } from './Island/Island'
@@ -13,94 +7,89 @@ import { useControls, folder } from 'leva'
 import { Sea } from './Sea/Sea'
     
     
-    export function Alpha() {
-        const {
-            islandColorRatio,
-            islandScale,
-            seaHeight,
-            seaFrequency,
-            seaSpeed,
-            seaTroughColor,
-            seaCrestColor,
-            seaWaveColorOffset,
-            seaWaveFactor,
-            seaNoiseFactor,
-            seaWaveAmplitude
-        } = useControls({
-            island: folder({
-                islandColorRatio: {
-                    label: "colorRatio",
-                    value: 0.5,
-                    min: 0,
-                    max: 1
-                },
-                islandScale: {
-                    label: "scale",
-                    value: 0.28,
-                    min: 0.1,
-                    max: 1
-                }
-            }),
-            sea: folder({
-                seaHeight: {
-                    label: "height",
-                    value: 0.02,
-                    min: 0.01,
-                    max: 1
-                },
-                seaFrequency: {
-                    label: "frequency",
-                    value: {x: 2, y: 2},
-                    min: 0,
-                    max: 4
-                },
-                seaSpeed: {
-                    label: "speed",
-                    value: 1,
-                    min: 0.1,
-                    max: 10
-                },
-                seaTroughColor: {
-                    label: "wave trough",
-                    value: '#061b5c'
-                },
-                seaCrestColor: {
-                    label: "wave crest",
-                    value: '#81d4de'
-                },
-                seaWaveColorOffset: {
-                    label: "color offset",
-                    value: 1.9,
-                    min: -2,
-                    max: 10
-                },
-                seaWaveFactor: {
-                    label: "color factor",
-                    value: 10,
-                    min: 1,
-                    max: 50
-                },
-                seaWaveAmplitude: {
-                    label: "noise amplitude",
-                    value: 0.7,
-                    min: 0.1,
-                    max: 10
-                },
-                seaNoiseFactor: {
-                    label: "noise factor",
-                    value: 0.6,
-                    min: 0.1,
-                    max: 10
-                }
-            })
+export function Alpha() {
+    const {
+        islandColorRatio,
+        islandScale,
+        seaHeight,
+        seaFrequency,
+        seaSpeed,
+        seaTroughColor,
+        seaCrestColor,
+        seaWaveColorOffset,
+        seaWaveFactor,
+        seaNoiseFactor,
+        seaWaveAmplitude
+    } = useControls({
+        island: folder({
+            islandColorRatio: {
+                label: "colorRatio",
+                value: 0.5,
+                min: 0,
+                max: 1
+            },
+            islandScale: {
+                label: "scale",
+                value: 0.28,
+                min: 0.1,
+                max: 1
+            }
+        }),
+        sea: folder({
+            seaHeight: {
+                label: "height",
+                value: 0.02,
+                min: 0.01,
+                max: 1
+            },
+            seaFrequency: {
+                label: "frequency",
+                value: {x: 2, y: 2},
+                min: 0,
+                max: 4
+            },
+            seaSpeed: {
+                label: "speed",
+                value: 1,
+                min: 0.1,
+                max: 10
+            },
+            seaTroughColor: {
+                label: "wave trough",
+                value: '#061b5c'
+            },
+            seaCrestColor: {
+                label: "wave crest",
+                value: '#81d4de'
+            },
+            seaWaveColorOffset: {
+                label: "color offset",
+                value: 1.9,
+                min: -2,
+                max: 10
+            },
+            seaWaveFactor: {
+                label: "color factor",
+                value: 10,
+                min: 1,
+                max: 50
+            },
+            seaWaveAmplitude: {
+                label: "noise amplitude",
+                value: 0.7,
+                min: 0.1,
+                max: 10
+            },
+            seaNoiseFactor: {
+                label: "noise factor",
+                value: 0.6,
+                min: 0.1,
+                max: 10
+            }
         })
+    })
 
-        usePotreeLoader('https://assets.h3nr7.com/downsampled_test_23-03-2023.las_converted')    
-      
-      // const rotation = new Euler()
-      // const quad = new Quaternion()
-      // quad.setFromAxisAngle(new Vector3(1, 0, 0), degToRad(-90))
-      // const r = rotation.setFromQuaternion(quad)
+    // usePotreeLoader('https://assets.h3nr7.com/downsampled_test_23-03-2023.las_converted')    
       
 
     return (
--- src/pages/Alpha/Sea/shaders/vert.ts
+++ src/pages/Alpha/Sea/shaders/vert.ts
@@ -21,28 +21,10 @@ export const vert = glsl`
 
   void main() {
 
-    // vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    
-    // float elevation = sin(modelPosition.x * seaFrequency.x + uTime * seaSpeed) *
-    //                   sin(modelPosition.y * seaFrequency.y + uTime * seaSpeed) *
-    //                   seaHeight;
-
-    // elevation += cnoise(vec3(modelPosition.xz, uTime * seaNoiseFactor));
-
-    // modelPosition.y += elevation;
-
-    // vec4 viewPosition = viewMatrix * modelPosition;
-    // vec4 projectedPosition = projectionMatrix * viewPosition;
-
-
-    
-    // gl_Position = projectedPosition;
-
-    // vElevation = elevation;
-
     vUv = uv;
-
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    // color map from the island png, so the sea would be static under the island
     float colorMapAlpha = texture2D(colorMap, vUv).a;
 
     // large waves according to normal vector
@@ -52,31 +34,21 @@ export const vert = glsl`
                            seaHeight;
 
     // smaller waves according to position
-    // float smallElevations = abs(uTime * cnoise(vec3(
-    //                           0.01 * seaNoiseFactor * length(normal.xz),
-    //                           0.01 * seaNoiseFactor * length(normal.yz),
-    //                           0.01 * seaNoiseFactor * length(normal.xy)
-    //                         )) * 0.15);
-
     float smallElevations = abs(turbulence(0.1 * seaNoiseFactor * normal * uTime)) * 0.05 * seaWaveAmplitude;
 
+    // adding the large wave and small waves
     float elevation = mainElevations + smallElevations;
-
 
     if(colorMapAlpha == 0.0) {
       modelPosition -= elevation;
     }
 
-
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
-
-
+    // out gl position
     gl_Position = projectedPosition;
-
-    // gl_Position = projectionMatrix * modelViewMatrix * vec4(normal, 1.0);
-
+    // pass elevation to fragment shader
     vElevation = elevation;
   }
 
