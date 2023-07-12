0100f27ac5a32b6e2c7bd357e25cf1f0017fc884
Henry YP Ho <me@h3nr7.com>
2023-07-12 22:51:50 +0100

    updated

--- src/elements/Title.tsx
+++ src/elements/Title.tsx
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 import { FontData, Text3D } from "@react-three/drei";
 import { PropsWithChildren, Ref, forwardRef, useImperativeHandle, useRef } from "react";
 import { Group, Mesh, Quaternion, Vector3 } from "three";
-=======
-import { Center, FontData, Text3D } from "@react-three/drei";
-import { Component, PropsWithChildren } from "react";
->>>>>>> 8310af26c6f8da00d888db71f4ea5082499810ec
 
 const DEFAULT_FONT_URL = `astor_place_bold_regular.json`
 
 interface ITitle extends PropsWithChildren {
-<<<<<<< HEAD
     innerRef: Ref<Mesh>
     text: string
     size?: number
@@ -46,7 +40,6 @@ export const Title = forwardRef<ITitleHandler, ITitle>(({
     }))
         
     return (
-        <group ref={groupRef}>
         <group 
             position={new Vector3(0, 7, 0)}
         >
@@ -56,28 +49,10 @@ export const Title = forwardRef<ITitleHandler, ITitle>(({
             bevelEnabled={bevelEnabled}
             font={fonts || DEFAULT_FONT_URL}
             size={size}>
-=======
-    text: string
-    fonts?: string | FontData
-}
-
-export function Title({ text, fonts, children }: ITitle) {
-
-    return (
-        <Center>
-        <Text3D font={fonts || DEFAULT_FONT_URL}>
->>>>>>> 8310af26c6f8da00d888db71f4ea5082499810ec
             {text && text.toUpperCase()}
             <meshNormalMaterial />
             {children}
             </Text3D> 
-<<<<<<< HEAD
-        </group>
         </group>
     )
 })
-=======
-        </Center>
-    )
-}
->>>>>>> 8310af26c6f8da00d888db71f4ea5082499810ec
