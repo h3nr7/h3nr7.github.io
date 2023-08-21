a4f882c96c63a04dc112e0cd5b9a929db6307146
Henry YP Ho <me@h3nr7.com>
2023-08-21 23:21:10 +0100

    working version

--- src/three/loaders/usePotreeLoader.tsx
+++ src/three/loaders/usePotreeLoader.tsx
@@ -1,27 +1,27 @@
-import { PointCloudOctree, Potree } from 'potree-loader'
+// import { PointCloudOctree, Potree } from 'potree-loader'
 import { useEffect, useState } from 'react'
 
 
 export function usePotreeLoader(url: string) {
-  const potree = new Potree()
-  const [pointcloud, setPointcloud] = useState<PointCloudOctree>()
+  // const potree = new Potree()
+  // const [pointcloud, setPointcloud] = useState<PointCloudOctree>()
   
   useEffect(() => {
 
-    async function pcLoad() {
-      const pco = await potree.loadPointCloud(
-          // The name of the point cloud which is to be loaded.
-          'metadata.json',
-          // Given the relative URL of a file, should return a full URL (e.g. signed).
-          relativeUrl => `${url}/${relativeUrl}`
-      )
+  //   async function pcLoad() {
+  //     const pco = await potree.loadPointCloud(
+  //         // The name of the point cloud which is to be loaded.
+  //         'metadata.json',
+  //         // Given the relative URL of a file, should return a full URL (e.g. signed).
+  //         relativeUrl => `${url}/${relativeUrl}`
+  //     )
 
-      setPointcloud(pco)
-    }
+  //     setPointcloud(pco)
+  //   }
 
-    pcLoad()
+  //   pcLoad()
 
   }, [url])
 
-  return { pointcloud }
+  // return { pointcloud }
 }
